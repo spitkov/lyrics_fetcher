1f3db71b96fa63736515fea254972f1620b5e76b
Bence <github@spitkov.hu>
2025-01-04 18:05:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "lyrics_fetcher",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
